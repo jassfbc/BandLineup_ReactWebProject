35ae02fc8b5533b1e5c386663b560a909b79ef94
Yasmine Ferjani <145272750+jassfbc@users.noreply.github.com>
2025-04-19 01:34:56 +0100

    Timeline_css_try

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,20 +1,58 @@
 import { Link } from "react-router-dom";
 import BandsInfo from "../data/BandsInfo"
+import "./Timeline.css"
 
 
 
 export default function Timeline(){
     return(
-        <div>
-            <h1> Jam Session Timeline</h1>
-            <ul>
-                {BandsInfo.map((band) => (
-                    <li key={band.id}>
-                        {band.time} - <Link to={`/band/${band.id}`}>{band.name}</Link>
-                    </li>
-                ))}
+        <div className="contain">
+            <div className="scroll-view">
+                <img
+                src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/jKP1lQciWn/czex1cz5_expires_30_days.png"} 
+                className="image"
+                />
 
-            </ul>
+                <span   span className="text_title" >
+                    {"Timeline"}
+                </span>
+                <ul>
+                    {BandsInfo.map((band) => (
+                        <div className="row_view">
+                            <span className="text" >
+                                {band.time}
+                            </span>
+                            <div className="box"></div>
+                            <Link className="text2" to={`/band/${band.id}`}>{band.name}</Link>
+
+                            <img
+                                src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/jKP1lQciWn/gj4y92kq_expires_30_days.png"} 
+                                className="image_line"
+                            />
+                        </div>
+                    ))}
+
+                </ul>
+
+                
+
+                <span className="text_title" >
+                    {"Meet Our Sponsors"}
+                </span>
+
+                <div className="sponso_logos">
+                <img
+                    src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/jKP1lQciWn/bs7tx8vx_expires_30_days.png"} 
+                    className="image3"
+                />
+                <img
+                    src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/jKP1lQciWn/3ca4mkk2_expires_30_days.png"} 
+                    className="image4"
+                />
+            </div>
+
+            </div>
+            
         </div>
     )
 }
